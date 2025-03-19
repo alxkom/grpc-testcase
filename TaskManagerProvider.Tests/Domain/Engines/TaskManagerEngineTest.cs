b1d290e58c1fd59ded144f778c1efbe4e26c66be
alxkom <alxkom018@gmail.com>
2025-03-19 13:55:27 +0300

    additional unit test case was added and other minor code amendments.

--- TaskManagerProvider.Tests/Domain/Engines/TaskManagerEngineTest.cs
+++ TaskManagerProvider.Tests/Domain/Engines/TaskManagerEngineTest.cs
@@ -96,4 +96,49 @@ public class TaskManagerEngineTest : IDisposable
                 (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()),
             Times.Once);
     }
+
+    [Fact]
+    public async TTask TaskManagerEngine_FailToDeleteUserAndRestoreAllRelatedTasks()
+    {
+        const int testUserId = 1;
+
+        var relatedTasks = new List<Task>([
+            new Task { Id = 1, Name = "Complete project", UserId = 1, State = (global::TaskManager.Models.TaskState)TaskState.New },
+            new Task { Id = 2, Name = "Review code", UserId = 1, State = (global::TaskManager.Models.TaskState)TaskState.InProgress },
+            new Task { Id = 3, Name = "Write documentation", UserId = 2, State = (global::TaskManager.Models.TaskState)TaskState.New }
+        ]).Where(task => task.UserId == testUserId).ToList();
+
+        var relatedTasksCount = relatedTasks.Count(t => t.UserId == testUserId);
+
+        _taskRepositoryMock.Setup(x => x.GetUserTasks(It.Is<int>(t => t == testUserId))).Returns(TTask.FromResult(relatedTasks.AsEnumerable()));
+        _taskRepositoryMock.Setup(x => x.DeleteTasks(It.IsAny<IEnumerable<Task>>())).Returns(TTask.FromResult(relatedTasks.Select(t => true).ToArray()));
+        _taskRepositoryMock.Setup(x => x.CreateTask(It.IsAny<Task>())).Returns((Task task) => TTask.FromResult(task));
+        _userRepositoryMock.Setup(x => x.DeleteUser(It.Is<int>(id => id == testUserId))).Returns(TTask.FromResult(false));
+
+        var result = await _taskManagerEngine.DeleteUser(testUserId);
+
+        Assert.False(result, "The user with the specified Id and all related tasks should be successfully restored.");
+        _taskRepositoryMock.Verify(x => x.GetUserTasks(It.Is<int>(t => t == testUserId)), Times.Once());
+        _taskRepositoryMock.Verify(x => x.DeleteTasks(It.IsAny<IEnumerable<Task>>()), Times.Once());
+        _taskRepositoryMock.Verify(x => x.CreateTask(It.IsAny<Task>()), Times.Exactly(relatedTasksCount));
+        _userRepositoryMock.Verify(x => x.DeleteUser(It.Is<int>(id => id == testUserId)), Times.Once);
+
+        _loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((o, t) => string.Equals($"{relatedTasksCount} related tasks were deleted for user with the specifed user Id: {testUserId}", o.ToString(), StringComparison.InvariantCultureIgnoreCase)),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()),
+            Times.Once);
+
+        _loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Warning,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((o, t) => string.Equals($"All related tasks were restored for user with the specifed user Id: {testUserId}", o.ToString(), StringComparison.InvariantCultureIgnoreCase)),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()),
+            Times.Once);
+    }
 }
